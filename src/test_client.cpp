b00f9b3acf64ce333b5bbb0525502c9559323905
Dale Baker <dlb74@uclive.ac.nz>
2017-07-13 13:03:53 +1200

    updated client so that it now accepts pointcloud images

--- src/test_client.cpp
+++ src/test_client.cpp
@@ -1,30 +1,29 @@
 #include "ros/ros.h"
 #include "branch_detection/AddTwoInts.h"
 #include <cstdlib>
+#include "std_msgs/String.h"
+#include <sstream>
+#include <sensor_msgs/PointCloud2.h>
+
+void chatterCallback(sensor_msgs::PointCloud2ConstPtr msg)
+{
+    //ROS_INFO("I heard: [%s]", msg->data.c_str());
+    ROS_INFO("here");
+
+}
+
 
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "test_client");
-    if (argc != 3)
-    {
-      ROS_INFO("usage: test_client X Y");
-      return 1;
-    }
 
     ros::NodeHandle n;
     ros::ServiceClient client = n.serviceClient<branch_detection::AddTwoInts>("add_two_ints");
     branch_detection::AddTwoInts srv;
-    srv.request.a = atoll(argv[1]);
-    srv.request.b = atoll(argv[2]);
-    if (client.call(srv))
-    {
-        ROS_INFO("Sum: %ld", (long int)srv.response.sum);
-    }
-    else
-    {
-        ROS_ERROR("Failed to call service add_two_ints");
-        return 1;
-    }
+
+    ros::Subscriber sub = n.subscribe<sensor_msgs::PointCloud2>("camera/depth/points", 1000, chatterCallback);
+    
+    ros::spin();
 
     return 0;
 }
