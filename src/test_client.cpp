b0612064921e1b40b578604712a6d3394e6d46e7
Dale Baker <dlb74@uclive.ac.nz>
2017-07-27 13:07:36 +1200

    added trunkseg method from Tims code

--- src/test_client.cpp
+++ src/test_client.cpp
@@ -43,8 +43,8 @@
 #define BRANCHSEG_NORMDIST_WEIGHT 0.05
 #define BRANCHSEG_MAXITT 1000
 #define BRANCHSEG_CYLDIST_THRESH 0.01
-#define BRANCHSEG_CYLRAD_MIN 0.002
-#define BRANCHSEG_CYLRAD_MAX 0.025
+#define BRANCHSEG_CYLRAD_MIN 0.05
+#define BRANCHSEG_CYLRAD_MAX 0.10
 
 typedef pcl::PointXYZ PointT;
 typedef pcl::Normal PointNT;
@@ -74,6 +74,105 @@ void Frame_Filter( pcl::PointCloud<PointT>::Ptr cloud,
     sor.setStddevMulThresh (KMEANFILTER_THRESH_STDVMUL);
     sor.filter (*cloud_filtered);
 }
+
+int Trunk_Seg( pcl::PointCloud<PointT>::Ptr cloud,
+               pcl::PointCloud<PointNT>::Ptr cloud_normals,
+               pcl::ModelCoefficients::Ptr coefficients_cylinder,
+               pcl::PointCloud<PointT>::Ptr cloud_remainder,
+               Eigen::Vector3f sample_COM )
+{
+    pcl::PointIndices::Ptr trunk_inliers (new pcl::PointIndices);
+    pcl::SACSegmentationFromNormals<PointT, PointNT> seg;
+    pcl::ExtractIndices<PointT> extract;
+    Eigen::Vector3f ax(0, 1, 0);
+    pcl::PointCloud<PointT>::Ptr trunk_branch_plane (new pcl::PointCloud<PointT> ());
+    pcl::PointCloud<PointT>::Ptr hull_points (new pcl::PointCloud<PointT> ());
+    pcl::ConvexHull<PointT> hull;
+
+    // Create the segmentation object for cylinder segmentation and set all the parameters
+    seg.setOptimizeCoefficients (true);
+    seg.setModelType (pcl::SACMODEL_CYLINDER);
+    seg.setAxis( ax );
+    seg.setEpsAngle ( TRUNKSEG_CYLDEV_MAX );
+    seg.setMethodType (pcl::SAC_RANSAC);
+    seg.setNormalDistanceWeight (TRUNKSEG_NORMDIST_WEIGHT);
+    seg.setMaxIterations (TRUNKSEG_MAXITT);
+    seg.setDistanceThreshold (TRUNKSEG_CYLDIST_THRESH);
+    seg.setRadiusLimits (TRUNKSEG_CYLRAD_MIN, TRUNKSEG_CYLRAD_MAX);
+    seg.setInputCloud (cloud);
+    seg.setInputNormals (cloud_normals);
+
+    // Obtain the cylinder inliers and coefficients
+    seg.segment (*trunk_inliers, *coefficients_cylinder);
+
+    Eigen::Vector3f axis(coefficients_cylinder->values[3],
+                         coefficients_cylinder->values[4],
+                         coefficients_cylinder->values[5]);
+    if (coefficients_cylinder->values[4] < 0) {
+        axis = -axis;
+        coefficients_cylinder->values[3] = -coefficients_cylinder->values[3];
+        coefficients_cylinder->values[4] = -coefficients_cylinder->values[4];
+        coefficients_cylinder->values[5] = -coefficients_cylinder->values[5];
+    }
+
+    Eigen::Vector3f trunkToCOM;
+    Eigen::Vector3f trunkNorm1;
+    Eigen::Vector3f COMNorm1;
+    Eigen::Vector3f trunkNorm2;
+    Eigen::Vector3f COMNorm2;
+    Eigen::Vector3f axis_point(coefficients_cylinder->values[0],
+                               coefficients_cylinder->values[1],
+                               coefficients_cylinder->values[2]);
+
+    trunkToCOM = (sample_COM - axis_point);
+    trunkNorm1 = trunkToCOM.cross(axis);
+    trunkNorm1.normalize();
+    trunkNorm1 = trunkNorm1*TRUNKEXTRACT_PRISM_RAD_MUL;
+    COMNorm1 = trunkNorm1*COMEXTRACT_PRISM_RAD_MUL;
+    trunkNorm2 = trunkNorm1.cross(axis);
+    trunkNorm2.normalize();
+    trunkNorm2 = trunkNorm2*TRUNKEXTRACT_PRISM_RAD_MUL;
+    COMNorm2 = trunkNorm2*COMEXTRACT_PRISM_RAD_MUL;
+
+    double trunk_radius = coefficients_cylinder->values[6];
+    for (int i = 0; i<3; i++)
+    {
+        PointT point;
+        point.x = axis_point(0) +
+                  ((-1)*(i==0)*1.74 + (i==1)*1.74)*trunk_radius*trunkNorm1(0) +
+                  ((-1)*(i==2)*2 + (i<2))*trunk_radius*trunkNorm2(0);
+        point.y = axis_point(1) +
+                  ((-1)*(i==0)*1.74 + (i==1)*1.74)*trunk_radius*trunkNorm1(1) +
+                  ((-1)*(i==2)*2 + (i<2))*trunk_radius*trunkNorm2(1);
+        point.z = axis_point(2) +
+                  ((-1)*(i==0)*1.74 + (i==1)*1.74)*trunk_radius*trunkNorm1(2) +
+                  ((-1)*(i==2)*2 + (i<2))*trunk_radius*trunkNorm2(2);
+        trunk_branch_plane->points.push_back(point);
+    }
+    trunk_branch_plane->width = (int)trunk_branch_plane->points.size();
+    trunk_branch_plane->height = 1;
+
+    std::vector< pcl::Vertices > polygons;
+    hull.setInputCloud (trunk_branch_plane);
+    hull.reconstruct (*hull_points, polygons);
+    if (hull.getDimension () == 2)
+    {
+        pcl::ExtractPolygonalPrismData<PointT> prism;
+        prism.setInputCloud (cloud);
+        prism.setInputPlanarHull (hull_points);
+        prism.setHeightLimits (-EXTRACT_PRISM_HEIGHT/2, EXTRACT_PRISM_HEIGHT/2);
+        prism.segment (*trunk_inliers);
+    }
+    else
+    {
+        PCL_ERROR ("The input cloud does not represent a planar surface.\n");
+    }
+    extract.setInputCloud (cloud);
+    extract.setIndices (trunk_inliers);
+    extract.setNegative (true);
+    extract.filter (*cloud_remainder);
+}
+
 
 void Branch_Seg( pcl::PointCloud<PointT>::Ptr cloud,
                  pcl::PointCloud<PointNT>::Ptr cloud_normals,
@@ -166,6 +265,7 @@ int main(int argc, char **argv)
 
     pcl::PointCloud<PointT>::Ptr cloud_DownSampled (new pcl::PointCloud<PointT>);
     Eigen::Vector3f COM;
+    pcl::PointCloud<PointT>::Ptr cloud_thresholded (new pcl::PointCloud<PointT>);
     pcl::PointCloud<PointT>::Ptr cloud_filtered (new pcl::PointCloud<PointT>);
     pcl::ModelCoefficients::Ptr coefficients_cylinder_branch (new pcl::ModelCoefficients);
     coefficients_cylinder_branch->values.resize (7);
@@ -181,23 +281,26 @@ int main(int argc, char **argv)
 
     while (!viewer->wasStopped ())
     {
+        cloud_thresholded->clear();
         cloud_DownSampled->clear();
         cloud_filtered->clear();
         branch_normals->clear();
         cloud_remainder->clear();
 
 
-        Filter_Far_Points( cloud, cloud_filtered );
-        cloud_filtered->width = (int)cloud_filtered->points.size();
+        Filter_Far_Points( cloud, cloud_thresholded );
+        cloud_thresholded->width = (int)cloud_thresholded->points.size();
 
-
-        DownSample( cloud_filtered, cloud_DownSampled );
+        DownSample( cloud_thresholded, cloud_DownSampled );
         cloud_DownSampled->width = (int)cloud_DownSampled->points.size();
 
-        Norm_Est( cloud_DownSampled, branch_normals, BRANCH_NORM_KSEARCH_RADIUS );
+        Frame_Filter( cloud_DownSampled, cloud_filtered );
+        cloud_filtered->width = (int)cloud_filtered->points.size();
+
+        Norm_Est( cloud_filtered, branch_normals, BRANCH_NORM_KSEARCH_RADIUS );
         branch_normals->width = (int)branch_normals->points.size();
 
-        Branch_Seg( cloud_DownSampled, branch_normals,
+        Branch_Seg( cloud_filtered, branch_normals,
                     coefficients_cylinder_branch, cloud_remainder);
 
         viewer->removeAllShapes();
