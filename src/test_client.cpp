aedcdf649e827c3c377b00ca88a05421a840f8c4
Dale Baker <dlb74@uclive.ac.nz>
2017-08-04 16:26:16 +1200

    cleaned up code and renamed variables and methods appropriately

--- src/test_client.cpp
+++ src/test_client.cpp
@@ -39,12 +39,12 @@
 #define KMEANFILTER_RANGE 10
 #define KMEANFILTER_THRESH_STDVMUL 0.8
 
-#define BRANCH_NORM_KSEARCH_RADIUS 0.05
-#define BRANCHSEG_NORMDIST_WEIGHT 0.05
-#define BRANCHSEG_MAXITT 1000
-#define BRANCHSEG_CYLDIST_THRESH 0.01
-#define BRANCHSEG_CYLRAD_MIN 0.05
-#define BRANCHSEG_CYLRAD_MAX 0.10
+#define TRUNK_NORM_KSEARCH_RADIUS 0.05
+#define TRUNKSEG_NORMDIST_WEIGHT 0.05
+#define TRUNKSEG_MAXITT 1000
+#define TRUNKSEG_CYLDIST_THRESH 0.01
+#define TRUNKSEG_CYLRAD_MIN 0.07
+#define TRUNKSEG_CYLRAD_MAX 0.10
 
 typedef pcl::PointXYZ PointT;
 typedef pcl::Normal PointNT;
@@ -75,26 +75,19 @@ void Frame_Filter( pcl::PointCloud<PointT>::Ptr cloud,
     sor.filter (*cloud_filtered);
 }
 
-/**
-int Trunk_Seg( pcl::PointCloud<PointT>::Ptr cloud,
-               pcl::PointCloud<PointNT>::Ptr cloud_normals,
-               pcl::ModelCoefficients::Ptr coefficients_cylinder,
-               pcl::PointCloud<PointT>::Ptr cloud_remainder,
-               Eigen::Vector3f sample_COM )
+
+void Trunk_Seg( pcl::PointCloud<PointT>::Ptr cloud,
+                 pcl::PointCloud<PointNT>::Ptr cloud_normals,
+                 pcl::ModelCoefficients::Ptr coefficients_cylinder,
+                 pcl::PointCloud<PointT>::Ptr cloud_remainder)
 {
-    pcl::PointIndices::Ptr trunk_inliers (new pcl::PointIndices);
+    pcl::PointIndices::Ptr inliers_trunk (new pcl::PointIndices);
     pcl::SACSegmentationFromNormals<PointT, PointNT> seg;
     pcl::ExtractIndices<PointT> extract;
-    Eigen::Vector3f ax(0, 1, 0);
-    pcl::PointCloud<PointT>::Ptr trunk_branch_plane (new pcl::PointCloud<PointT> ());
-    pcl::PointCloud<PointT>::Ptr hull_points (new pcl::PointCloud<PointT> ());
-    pcl::ConvexHull<PointT> hull;
 
     // Create the segmentation object for cylinder segmentation and set all the parameters
     seg.setOptimizeCoefficients (true);
     seg.setModelType (pcl::SACMODEL_CYLINDER);
-    seg.setAxis( ax );
-    seg.setEpsAngle ( TRUNKSEG_CYLDEV_MAX );
     seg.setMethodType (pcl::SAC_RANSAC);
     seg.setNormalDistanceWeight (TRUNKSEG_NORMDIST_WEIGHT);
     seg.setMaxIterations (TRUNKSEG_MAXITT);
@@ -104,99 +97,7 @@ int Trunk_Seg( pcl::PointCloud<PointT>::Ptr cloud,
     seg.setInputNormals (cloud_normals);
 
     // Obtain the cylinder inliers and coefficients
-    seg.segment (*trunk_inliers, *coefficients_cylinder);
-
-    Eigen::Vector3f axis(coefficients_cylinder->values[3],
-                         coefficients_cylinder->values[4],
-                         coefficients_cylinder->values[5]);
-    if (coefficients_cylinder->values[4] < 0) {
-        axis = -axis;
-        coefficients_cylinder->values[3] = -coefficients_cylinder->values[3];
-        coefficients_cylinder->values[4] = -coefficients_cylinder->values[4];
-        coefficients_cylinder->values[5] = -coefficients_cylinder->values[5];
-    }
-
-    Eigen::Vector3f trunkToCOM;
-    Eigen::Vector3f trunkNorm1;
-    Eigen::Vector3f COMNorm1;
-    Eigen::Vector3f trunkNorm2;
-    Eigen::Vector3f COMNorm2;
-    Eigen::Vector3f axis_point(coefficients_cylinder->values[0],
-                               coefficients_cylinder->values[1],
-                               coefficients_cylinder->values[2]);
-
-    trunkToCOM = (sample_COM - axis_point);
-    trunkNorm1 = trunkToCOM.cross(axis);
-    trunkNorm1.normalize();
-    trunkNorm1 = trunkNorm1*TRUNKEXTRACT_PRISM_RAD_MUL;
-    COMNorm1 = trunkNorm1*COMEXTRACT_PRISM_RAD_MUL;
-    trunkNorm2 = trunkNorm1.cross(axis);
-    trunkNorm2.normalize();
-    trunkNorm2 = trunkNorm2*TRUNKEXTRACT_PRISM_RAD_MUL;
-    COMNorm2 = trunkNorm2*COMEXTRACT_PRISM_RAD_MUL;
-
-    double trunk_radius = coefficients_cylinder->values[6];
-    for (int i = 0; i<3; i++)
-    {
-        PointT point;
-        point.x = axis_point(0) +
-                  ((-1)*(i==0)*1.74 + (i==1)*1.74)*trunk_radius*trunkNorm1(0) +
-                  ((-1)*(i==2)*2 + (i<2))*trunk_radius*trunkNorm2(0);
-        point.y = axis_point(1) +
-                  ((-1)*(i==0)*1.74 + (i==1)*1.74)*trunk_radius*trunkNorm1(1) +
-                  ((-1)*(i==2)*2 + (i<2))*trunk_radius*trunkNorm2(1);
-        point.z = axis_point(2) +
-                  ((-1)*(i==0)*1.74 + (i==1)*1.74)*trunk_radius*trunkNorm1(2) +
-                  ((-1)*(i==2)*2 + (i<2))*trunk_radius*trunkNorm2(2);
-        trunk_branch_plane->points.push_back(point);
-    }
-    trunk_branch_plane->width = (int)trunk_branch_plane->points.size();
-    trunk_branch_plane->height = 1;
-
-    std::vector< pcl::Vertices > polygons;
-    hull.setInputCloud (trunk_branch_plane);
-    hull.reconstruct (*hull_points, polygons);
-    if (hull.getDimension () == 2)
-    {
-        pcl::ExtractPolygonalPrismData<PointT> prism;
-        prism.setInputCloud (cloud);
-        prism.setInputPlanarHull (hull_points);
-        prism.setHeightLimits (-EXTRACT_PRISM_HEIGHT/2, EXTRACT_PRISM_HEIGHT/2);
-        prism.segment (*trunk_inliers);
-    }
-    else
-    {
-        PCL_ERROR ("The input cloud does not represent a planar surface.\n");
-    }
-    extract.setInputCloud (cloud);
-    extract.setIndices (trunk_inliers);
-    extract.setNegative (true);
-    extract.filter (*cloud_remainder);
-}
-*/
-
-void Branch_Seg( pcl::PointCloud<PointT>::Ptr cloud,
-                 pcl::PointCloud<PointNT>::Ptr cloud_normals,
-                 pcl::ModelCoefficients::Ptr coefficients_cylinder,
-                 pcl::PointCloud<PointT>::Ptr cloud_remainder )
-{
-    pcl::PointIndices::Ptr inliers_branch (new pcl::PointIndices);
-    pcl::SACSegmentationFromNormals<PointT, PointNT> seg;
-    pcl::ExtractIndices<PointT> extract;
-
-    // Create the segmentation object for cylinder segmentation and set all the parameters
-    seg.setOptimizeCoefficients (true);
-    seg.setModelType (pcl::SACMODEL_CYLINDER);
-    seg.setMethodType (pcl::SAC_RANSAC);
-    seg.setNormalDistanceWeight (BRANCHSEG_NORMDIST_WEIGHT);
-    seg.setMaxIterations (BRANCHSEG_MAXITT);
-    seg.setDistanceThreshold (BRANCHSEG_CYLDIST_THRESH);
-    seg.setRadiusLimits (BRANCHSEG_CYLRAD_MIN, BRANCHSEG_CYLRAD_MAX);
-    seg.setInputCloud (cloud);
-    seg.setInputNormals (cloud_normals);
-
-    // Obtain the cylinder inliers and coefficients
-    seg.segment (*inliers_branch, *coefficients_cylinder);
+    seg.segment (*inliers_trunk, *coefficients_cylinder);
 
     if (coefficients_cylinder->values[4] < 0) {
         Eigen::Matrix3f R;
@@ -214,7 +115,7 @@ void Branch_Seg( pcl::PointCloud<PointT>::Ptr cloud,
 
     // Extract cylinder inliers
     extract.setInputCloud (cloud);
-    extract.setIndices (inliers_branch);
+    extract.setIndices (inliers_trunk);
     extract.setNegative (true);
     extract.filter (*cloud_remainder);
 }
@@ -268,9 +169,9 @@ int main(int argc, char **argv)
     Eigen::Vector3f COM;
     pcl::PointCloud<PointT>::Ptr cloud_thresholded (new pcl::PointCloud<PointT>);
     pcl::PointCloud<PointT>::Ptr cloud_filtered (new pcl::PointCloud<PointT>);
-    pcl::ModelCoefficients::Ptr coefficients_cylinder_branch (new pcl::ModelCoefficients);
-    coefficients_cylinder_branch->values.resize (7);
-    pcl::PointCloud<PointNT>::Ptr branch_normals (new pcl::PointCloud<PointNT>);
+    pcl::ModelCoefficients::Ptr coefficients_cylinder_trunk (new pcl::ModelCoefficients);
+    coefficients_cylinder_trunk->values.resize (7);
+    pcl::PointCloud<PointNT>::Ptr trunk_normals (new pcl::PointCloud<PointNT>);
     pcl::PointCloud<PointT>::Ptr cloud_remainder (new pcl::PointCloud<PointT> ());
 
 
@@ -292,7 +193,7 @@ int main(int argc, char **argv)
         cloud_thresholded->clear();
         cloud_DownSampled->clear();
         cloud_filtered->clear();
-        branch_normals->clear();
+        trunk_normals->clear();
         cloud_remainder->clear();
 
         Filter_Far_Points( cloud, cloud_thresholded );
@@ -304,20 +205,20 @@ int main(int argc, char **argv)
         Frame_Filter( cloud_DownSampled, cloud_filtered );
         cloud_filtered->width = (int)cloud_filtered->points.size();
 
-        Norm_Est( cloud_filtered, branch_normals, BRANCH_NORM_KSEARCH_RADIUS );
-        branch_normals->width = (int)branch_normals->points.size();
+        Norm_Est( cloud_filtered, trunk_normals, TRUNK_NORM_KSEARCH_RADIUS );
+        trunk_normals->width = (int)trunk_normals->points.size();
 
-        Branch_Seg( cloud_filtered, branch_normals,
-                    coefficients_cylinder_branch, cloud_remainder);
+        Trunk_Seg( cloud_filtered, trunk_normals,
+                   coefficients_cylinder_trunk, cloud_remainder);
 
-        //if (coefficients_cylinder_branch->values[0] != 0) {stopper = 1;}
+        //if (coefficients_cylinder_trunk->values[0] != 0) {stopper = 1;}
 
         //if (stopper == 1) {
 
 
             viewer->removeAllShapes();
             viewer->updatePointCloud(cloud_filtered, "Filtered Cloud");
-            viewer->addCylinder(*coefficients_cylinder_branch, "sadfsaf");
+            viewer->addCylinder(*coefficients_cylinder_trunk, "sadfsaf");
             viewer->spinOnce(100);
 
             viewer2->removeAllShapes();
